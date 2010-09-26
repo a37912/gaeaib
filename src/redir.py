2fca17cf436f9e76454ac7a9f427d3561760bf1a
Ivan Deep <vasyai@bk.ru>
2010-09-26 13:27:58 +0300

    remove redir from gaech

--- src/redir.py
+++ src/redir.py
@@ -2,7 +2,7 @@ import logging
 from tipfy import redirect, get_config
 
 REDIRECT = "http://www.bloop.ru"
-FRM = ('gaeaib.appspot.com', 'www.gaech.org')
+FRM = ('gaeaib.appspot.com',)
 class RedirMW(object):
   def pre_dispatch(self, handler):
     if get_config("tipfy", "dev"):
