ea5cb1f05dbbf4be10ea78080b676afdbbf76319
Ivan Deep <vasyai@bk.ru>
2010-10-03 13:40:23 +0300

    increment render

---
+++ src/aib/render.py
@@ -0,0 +1,80 @@
+from models import Cache
+from tipfy.ext.jinja2 import render_template
+from const import *
+from mark import markup
+import re
+
+class Render(object):
+  MAXREFS = 5
+  def __init__(self, board, thread):
+    self.board = board
+    self.thread = thread
+
+  def load(self,):
+    self.html = Cache.load(Board=self.board, Thread=self.thread)
+
+    if self.html:
+      self.html = self.html.decode('utf8')
+
+
+  def _markup(self, post):
+    post = post.copy()
+    post['html'] = markup(
+        board=self.board, postid=post.get("post"),
+        data = post.get('text'),
+    )
+
+    return post
+
+  def create(self,op):
+    op = self._markup(op)
+
+    data = {
+        "op" : op,
+        "id" : self.thread,
+        "board" : self.board,
+        "subject" : op.get("subject"),
+    }
+    self.html = render_template("thread_new.html", 
+        threads = (data,),
+        board = self.board,
+        board_name = boardlist.get(self.board, "Woooo???"),
+        boards = boardlist_order,
+    )
+
+  def add(self,post,new=False):
+    if new:
+      self.create(post)
+    else:
+      self.load()
+      self.append(post)
+
+  def append(self, post):
+    assert self.html, 'nowhere to append'
+
+    post = self._markup(post)
+
+    post_html = render_template("post.html", post=post)
+
+    self.html = self.html.replace(
+        u"<!--NPHERE-->", post_html)
+
+    refs = re.findall(">>([0-9]+)", post.get("text", "")) or []
+
+    if refs:
+      refhtml = render_template("ref.html", 
+          board = self.board,
+          thread = self.thread,
+          refpost = post.get("post"),
+          rainbow = post.get("rainbow_html")
+      )
+
+    import logging
+    for ref in refs[:self.MAXREFS]:
+      logging.info("ref: %s, %s" % (ref, type(ref)))
+      pattern = u"<!--REF-%s-->" % ref
+      self.html = self.html.replace(pattern, refhtml+pattern)
+
+  def save(self):
+    Cache.save(self.html, Board=self.board, Thread=self.thread)
+
