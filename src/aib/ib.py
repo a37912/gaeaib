1da3b6a105fd276847f7016f86d4725dddc21394
xen <m@xen.ru>
2010-10-16 18:49:40 +0300

    some work

--- src/aib/ib.py
+++ src/aib/ib.py
@@ -49,6 +49,9 @@ class Board(RequestHandler):
     data['board'] = board # board name
     data['boards'] = boardlist_order
     data['pages'] = range(BOARD_PAGES)
+    if data['threads']:
+      data['op_thread'] = data['threads'][0]
+      data['threads'] = data['threads'][1:]
 
     html = render_template("thread.html", **data)
 
